70db2369392d8aee7054b86a7ee5ef40e036af14
Ogpavan <pawantwp@gmail.com>
2024-10-22 22:48:20 +0530

    dss

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,45 +1,45 @@
-import React from 'react';
+import React from  react ;
 
 export default function Page() {
     const timelineData = [
         {
-            year: '2014',
+            year:  '2014' ,
             content: `After Vishal Garg&apos;s first attempt to purchase his own dream home, he quickly realized that the homebuying process is unnecessarily broken. This inspired him to found a technology-first company led by engineers and data experts with the mission of digitizing and automating home finance to make it cheaper, easier, and faster for all.`,
         },
         {
-            year: '2015',
+            year:  '2015' ,
             content: `Better Mortgage funds its first mortgage loan entirely online (without a single phone call!).`,
         },
         {
-            year: '2016',
+            year:  '2016' ,
             content: `Better Mortgage becomes a Fannie Mae approved seller + servicer and establishes relationships with top mortgage investors.`,
         },
         {
-            year: '2017',
+            year:  '2017' ,
             content: `Better expands into the real estate market with Better Real Estate.`,
         },
         {
-            year: '2018',
+            year:  '2018' ,
             content: `Better Mortgage partners with Ally Bank to build Ally powered by Better.`,
         },
         {
-            year: '2019',
+            year:  '2019' ,
             content: `Better Mortgage launches its pilot partnership with American Express to deliver a seamless homebuying experience to AMEX customers.`,
         },
         {
-            year: '2021',
+            year:  '2021' ,
             content: `Better acquires Trussle — The UK&apos;s most innovative online mortgage broker.`,
         },
         {
-            year: '2022',
+            year:  '2022' ,
             content: `Better Mortgage becomes the first fintech to fund $100B home loans entirely online.`,
         },
         {
-            year: '2023',
+            year:  '2023' ,
             content: `Better Mortgage launches One Day Mortgage: The first offering to go from application to full mortgage commitment within 24 hours vs. typical 30+ days.`,
         },
         {
-            year: 'Today',
+            year:  'Today' ,
             content: `You become part of the story by joining tens of thousands of happy Better Mortgage borrowers. <a class="underline" href="/start">Get started</a>`,
         },
     ];
@@ -62,10 +62,10 @@ export default function Page() {
                     <div className="md:w-1/2 space-y-4 mb-6 md:mb-0">
                         <h1 className="text-2xl font-semibold">The status quo is broken</h1>
                         <p className="text-sm text-gray-800">
-                            The traditional processes around homeownership are opaque and stressful. Fees aren't transparent, and some are simply outrageous. Traditional mortgage lending is rife with unnecessary fees and slow, painful processes. It's a system set up to benefit insiders — not you. Better.com CEO, Vishal Garg, set out to change that.
+                            The traditional processes around homeownership are opaque and stressful. Fees aren t transparent, and some are simply outrageous. Traditional mortgage lending is rife with unnecessary fees and slow, painful processes. It s a system set up to benefit insiders — not you. Better.com CEO, Vishal Garg, set out to change that.
                         </p>
                         <button className="bg-[#186650] text-white px-4 py-2 rounded">
-                            Read Vishal's story
+                            Read Vishal s story
                         </button>
                     </div>
                     <div className="md:w-1/2">
@@ -89,7 +89,7 @@ export default function Page() {
                         <div
                             key={item.year}
                             className={`relative flex flex-col items-center ${
-                                index % 2 === 0 ? '-translate-x-1/2' : 'translate-x-1/2'
+                                index % 2 === 0 ?  -translate-x-1/2  :  translate-x-1/2 
                             }`}
                         >
                             <div className="inline-block rounded-full bg-[#004733] px-8 py-2 mb-4">
